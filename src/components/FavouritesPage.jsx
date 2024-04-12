1bad838cb2abcc22356d02817a7b75f97f14e0e8
Shadley91 <shadley.phil@gmail.com>
2024-04-12 13:06:13 +0200

    Updated the FavouritesPage component, so that the functionality for the sorting from A-Z and Z-A as well as sorting in Ascending and Descending order now works.

--- src/components/FavouritesPage.jsx
+++ src/components/FavouritesPage.jsx
@@ -1,39 +1,34 @@
-import { useState, useEffect } from "react"; // Importing necessary hooks from React
-import { useLocation } from "react-router-dom"; // Importing useLocation hook from React Router DOM
-import IconButton from "@mui/material/IconButton"; // Importing IconButton component from Material-UI
-import ArrowBackIcon from "@mui/icons-material/ArrowBack"; // Importing ArrowBackIcon component from Material-UI icons
-import Button from "@mui/material/Button"; // Importing Button component from Material-UI
-import Grid from "@mui/material/Grid"; // Importing Grid component from Material-UI
-import DeleteIcon from "@mui/icons-material/Delete"; // Importing the trash can icon from Material-UI icons
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import IconButton from "@mui/material/IconButton";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const FavouritesPage = () => {
-  // State variables for managing favourites and selected show
   const [favourites, setFavourites] = useState([]);
   const [selectedShow, setSelectedShow] = useState(null);
-  const location = useLocation(); // Using useLocation hook to get the current location
+  const location = useLocation();
 
   useEffect(() => {
-    // Load favorites from local storage when the component mounts
     const storedFavorites = JSON.parse(localStorage.getItem("favorites"));
     if (storedFavorites) {
       setFavourites(storedFavorites);
     }
   }, []);
 
-  const handleFavouritesBackButton = () => {
-    // Go back to the previous page when back button is clicked
-    window.history.back();
-  };
-
   useEffect(() => {
-    // Update selected show when location state changes
     if (location.state && location.state.show) {
       setSelectedShow(location.state.show);
     }
   }, [location.state]);
 
+  const handleFavouritesBackButton = () => {
+    window.history.back();
+  };
+
   const addToFavourites = () => {
-    // Add selected show to favourites
     if (selectedShow) {
       const showToAdd = selectedShow;
       const showExists = favourites.some((fav) => fav.id === showToAdd.id);
@@ -42,7 +37,7 @@ const FavouritesPage = () => {
           ...favourites,
           {
             id: showToAdd.id,
-            title: showToAdd.title, // Add title here
+            title: showToAdd.title,
             show: showToAdd.show,
             season: showToAdd.season,
             episode: showToAdd.episode,
@@ -58,30 +53,26 @@ const FavouritesPage = () => {
   };
 
   const removeFavourite = (id) => {
-    // Remove favourite by id
     const updatedFavourites = favourites.filter((fav) => fav.id !== id);
     setFavourites(updatedFavourites);
     localStorage.setItem("favorites", JSON.stringify(updatedFavourites));
   };
 
-  const sortByShowAZ = () => {
-    // Sort favourites alphabetically by show name (A-Z)
+  const sortFavouritesByTitleAZ = () => {
     const sortedFavourites = [...favourites].sort((a, b) =>
-      a.show.localeCompare(b.show)
+      a.title.toLowerCase().localeCompare(b.title.toLowerCase())
     );
     setFavourites(sortedFavourites);
   };
 
-  const sortByShowZA = () => {
-    // Sort favourites alphabetically by show name (Z-A)
+  const sortFavouritesByTitleZA = () => {
     const sortedFavourites = [...favourites].sort((a, b) =>
-      b.show.localeCompare(a.show)
+      b.title.toLowerCase().localeCompare(a.title.toLowerCase())
     );
     setFavourites(sortedFavourites);
   };
 
   const sortByDateAsc = () => {
-    // Sort favourites by date added in ascending order
     const sortedFavourites = [...favourites].sort(
       (a, b) => new Date(a.dateTimeAdded) - new Date(b.dateTimeAdded)
     );
@@ -89,7 +80,6 @@ const FavouritesPage = () => {
   };
 
   const sortByDateDesc = () => {
-    // Sort favourites by date added in descending order
     const sortedFavourites = [...favourites].sort(
       (a, b) => new Date(b.dateTimeAdded) - new Date(a.dateTimeAdded)
     );
@@ -98,7 +88,6 @@ const FavouritesPage = () => {
 
   return (
     <div>
-      {/* Back button to navigate back */}
       <IconButton
         onClick={handleFavouritesBackButton}
         color="primary"
@@ -107,16 +96,23 @@ const FavouritesPage = () => {
         <ArrowBackIcon />
       </IconButton>
       <h1>Favourites</h1>
-      {/* Buttons for sorting favourites */}
       <Grid container spacing={2}>
         <Grid item>
-          <Button onClick={sortByShowAZ} variant="contained" color="primary">
-            Sort by Show A-Z
+          <Button
+            onClick={sortFavouritesByTitleAZ}
+            variant="contained"
+            color="primary"
+          >
+            Sort by Title A-Z
           </Button>
         </Grid>
         <Grid item>
-          <Button onClick={sortByShowZA} variant="contained" color="primary">
-            Sort by Show Z-A
+          <Button
+            onClick={sortFavouritesByTitleZA}
+            variant="contained"
+            color="primary"
+          >
+            Sort by Title Z-A
           </Button>
         </Grid>
         <Grid item>
@@ -130,7 +126,6 @@ const FavouritesPage = () => {
           </Button>
         </Grid>
       </Grid>
-      {/* Display selected show if exists */}
       {selectedShow && (
         <div>
           <p>
@@ -142,37 +137,39 @@ const FavouritesPage = () => {
             alt={selectedShow.show}
             style={{ width: "200px" }}
           />
-          {/* Button to add selected show to favourites */}
           <Button onClick={addToFavourites} variant="contained" color="primary">
             Add to Favourites
           </Button>
         </div>
       )}
-      {/* Display list of favourites */}
-      {favourites.map((favourite) => (
-        <div key={favourite.id}>
-          <p>
-            {favourite.title} - {favourite.show} - Season {favourite.season},
-            Episode: {favourite.episode}
-          </p>
-          <img
-            src={favourite.image}
-            alt={favourite.show}
-            style={{ width: "200px" }}
-          />
-          <p>
-            Date and Time Added: {new Date(favourite.dateTimeAdded).toString()}
-          </p>
-          {/* Button to remove favourite */}
-          <IconButton
-            onClick={() => removeFavourite(favourite.id)}
-            color="primary"
-            aria-label="remove"
-          >
-            <DeleteIcon />
-          </IconButton>
-        </div>
-      ))}
+      {favourites.length > 0 ? (
+        favourites.map((favourite) => (
+          <div key={favourite.id}>
+            <p>
+              {favourite.title} - {favourite.show} - Season {favourite.season},
+              Episode: {favourite.episode}
+            </p>
+            <img
+              src={favourite.image}
+              alt={favourite.show}
+              style={{ width: "200px" }}
+            />
+            <p>
+              Date and Time Added:{" "}
+              {new Date(favourite.dateTimeAdded).toString()}
+            </p>
+            <IconButton
+              onClick={() => removeFavourite(favourite.id)}
+              color="primary"
+              aria-label="remove"
+            >
+              <DeleteIcon />
+            </IconButton>
+          </div>
+        ))
+      ) : (
+        <p>No favourites added yet.</p>
+      )}
     </div>
   );
 };
